7eb2b43e0044c1c2fd21e0a4a1323420e019d884
Radman R <113865339+redradman@users.noreply.github.com>
2024-08-10 03:11:08 -0700

    implement crypto_symbol_guard

--- data_fetcher.py
+++ data_fetcher.py
@@ -3,14 +3,50 @@
 from secret import *
 import pandas as pd 
 import ssl
+from enum import Enum
 
 # bypass certificate verification
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
 
+# address of the gaurds for checking the existance of passed strings
+CRYPTO_LIST = "digital_currency_list.csv"
+crypto_list_df = pd.read_csv(CRYPTO_LIST)
 
-# define class
+
+
+
+
+
+
+
+def crypto_symbol_guard(ticker: str):
+    """
+    Prevents wrong spelling by checking the passed string aginst database if it does not exist returns false
+    """
+    if ticker.upper() in crypto_list_df.values:
+        return True
+    return False
+
+
+class Timeframe(Enum):
+    """
+    An enum used for specifying timeframes and reducing errors
+    """
+    ONE_MINUTE = "1m"
+    FIVE_MINUTES = "5m"
+    FIFTEEN_MINUTES = "15m"
+    THIRTY_MINUTES = "30m"
+    ONE_HOUR = "1h"
+    FOUR_HOURS = "4h"
+    DAILY = "1d"
+    WEEKLY = "1w"
+    MONTHLY = "1mo"
+
+
+
+# define data_fetcher class called Hermes (Greek mythology lol)
 class Hermes:
     """
     Model used for getting all of the market data
@@ -19,6 +55,17 @@ class Hermes:
     def __init__(self):
         self.api_key = get_secret()
         self.base_url = get_base_url()
+        
+    def build_dataframe(symbol: str, 
+                        ):
+        
+        
+        """
+        
+        """
+        
+        
+        pass
     
         
     
